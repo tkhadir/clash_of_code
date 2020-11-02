d61e13aa27361a1a36a23698c803518eaec73c1b
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2020-11-02 16:06:01 +0100

    Create reverse_example.js

---
+++ reverse_example.js
@@ -0,0 +1,20 @@
+const number = parseInt(readline());
+
+
+console.error('number', number)
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const newNum = `${number}`.split('')
+
+console.error('newNum', newNum)
+
+let result = 0
+newNum.forEach((item) => {
+    result += parseInt(item)
+})
+
+result = result ** 2
+
+console.log(result)
