d9b95ed9632a60427dc6eea2518fdc317802b0ae
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2020-12-29 09:28:53 +0100

    Create brute_force.js

---
+++ brute_force.js
@@ -0,0 +1,35 @@
+const password = 'findme'
+const minLength = 7
+const maxLength = 8
+
+const prepare = (min, max) => {
+    let i=0
+    let minFormat = ''
+    let maxFormat = ''
+    while (i < min) {
+        minFormat+='a'
+        i++
+    }
+
+    i=0
+    while (i < max) {
+        maxFormat+='z'
+        i++
+    }
+    return {'minpass': minFormat, 'maxpass': maxFormat}
+}
+
+const passwordFormat = prepare(minLength, maxLength)
+console.log('possible password format : min :' + passwordFormat['minpass'] + ' max : ' + passwordFormat['maxpass'])
+
+console.log('start process time : ' + new Date())
+let i = parseInt(passwordFormat['minpass'], 36)
+while(i <= parseInt(passwordFormat['maxpass'], 36)) {
+    //console.log(i.toString(36))
+    if (password === i.toString(36)) {
+        console.log('found : ' + i.toString(36))
+        break
+    }
+    i++
+}
+console.log('start process time : ' + new Date())
